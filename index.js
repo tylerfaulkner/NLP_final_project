9daf3657902b7ab0dc9b071bb200af2fc25e18dc
TKT2Ride <56010171+TKT2Ride@users.noreply.github.com>
2023-05-11 14:18:27 -0500

    Added status message

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ url = "http://nlp-prototype.tyler-faulkner.com:5000/generateSummary";
 function generateSumm(button) {
   var text = button.textContent;
   text = text.replace(/ /g, "-");
+  document.getElementById("status").innerHTML = "Generating summary...";
 
   $.get(url, { filename: text }, function (data) {
     document.getElementById("summaryBox").innerHTML = text(data).html();
+    document.getElementById("status").innerHTML = "Done!";
   });
   console.log(text);
 }
